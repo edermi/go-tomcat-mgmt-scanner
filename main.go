7ed7acdec615656a8dedeed2cf5ecec7d77c68e7
Michael Eder <spam@michael-eder.net>
2018-08-26 23:36:16 +0200

    Large refactoring + CLI interface

--- main.go
+++ main.go
@@ -1,84 +1,111 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"net"
 	"net/http"
+	"sync"
 	"time"
 )
 
-type tcinstance struct {
-	host             string
-	port             int
-	managerPath      string
-	checked          bool
-	managerAvailable bool
-	finished         bool
-}
-
-// GET: /manager/html
-// HTTP Basic Auth
+var guesses []Guess
+var scannerConfig ScannerConfig
 
 func main() {
-	guesses := buildGuesses()
-	addrs := expandNetwork(&net.IPNet{IP: net.IPv4(127, 0, 0, 0), Mask: net.CIDRMask(24, 32)})
-	ports := [6]int{8080, 8443, 80, 443, 8000, 8888}
-	channel := make(chan tcinstance, 20)
-	for gonum := 0; gonum < 10; gonum++ {
+	scannerConfig = parseCommandLineArgs()
+	defer timeTrack(time.Now()) // Count execution time
+	guesses = buildGuesses()
+	workQueue := make(chan TcInstance, 20) // This channel is used to pass work to the goroutines
+	var wg sync.WaitGroup                  // The WaitGroup is used to wait for all goroutines to finish at the end
+	spawnWorkers(&wg, workQueue)
+	fillQueue(workQueue)
+	wg.Wait() // Wait for goroutines to finish
+}
+
+// This function fills the workQueue channel with tcinstances that are scanned
+// It also tracks process and reports status from time to time
+func fillQueue(workQueue chan<- TcInstance) {
+	numTargets := len(scannerConfig.targetRange) * len(scannerConfig.ports)
+	progress := 0
+	tenths := 1
+	for _, element := range scannerConfig.targetRange {
+		for _, port := range scannerConfig.ports {
+			if progress > tenths*(numTargets/10) {
+				log.Printf("[*] ~%d0%% (%d/%d)\n", tenths, progress, numTargets)
+				tenths++
+			}
+			tc := TcInstance{element.String(), port, scannerConfig.managerPath, false, false, false}
+			workQueue <- tc
+			progress++
+		}
+	}
+	close(workQueue) // The closed channel also tells the goroutines to return
+}
+
+// spawnWorkers creates the desired number of goroutines, adds them to the WaitGroup and receives work from the channel
+func spawnWorkers(wg *sync.WaitGroup, workQueue <-chan TcInstance) {
+	for worker := uint(0); worker < scannerConfig.goroutines; worker++ {
+		wg.Add(1)
 		go func() {
-			tc := <-channel
-			tc.check()
-			for _, guess := range guesses {
-				success, _ := request(&tc, guess.username, guess.password)
-				if success {
+			defer wg.Done()
+			client := &http.Client{ // We need to initialise our own http client struct
+				Timeout: time.Second * 3, // in order to define our own timeout
+			}
+			for {
+				tc, ok := <-workQueue
+				if ok == false { // If the channel is closed, we are done
 					break
+				}
+				tc.check(client, false)
+				for _, guess := range guesses {
+					success, _ := request(&tc, guess.username, guess.password)
+					if success {
+						break
+					}
 				}
 			}
 		}()
 	}
-	for _, element := range addrs {
-		for _, port := range ports {
-			tc := tcinstance{element.String(), port, "/manager/html", false, false, false}
-			go func() { channel <- tc }()
-
-		}
-	}
-
 }
 
-func (tc *tcinstance) check() {
-	client := &http.Client{
-		Timeout: time.Second * 3,
+func (tc *TcInstance) check(client *http.Client, TLSenabled bool) {
+	//log.Printf("[-] Checking %s\n", buildRequestURL(false, tc.host, tc.port, tc.managerPath))
+	var resp *http.Response
+	var err error
+	if TLSenabled {
+		resp, err = client.Get(buildRequestURL(true, tc.host, tc.port, tc.managerPath))
+	} else {
+		resp, err = client.Get(buildRequestURL(false, tc.host, tc.port, tc.managerPath))
 	}
-	log.Printf("[-] Checking %s\n", buildRequestURL(false, tc.host, tc.port, tc.managerPath))
-
-	resp, err := client.Get(buildRequestURL(false, tc.host, tc.port, tc.managerPath))
 	if err != nil {
+		//log.Println(err.Error()) // For debugging socket issues
 		tc.checked = true
 		tc.finished = true
 		return
 	}
+	defer resp.Body.Close()
 	if resp.StatusCode == http.StatusNotFound {
 		log.Printf("[-] Manager not found at %s:%d%s\n", tc.host, tc.port, tc.managerPath)
 		tc.managerAvailable = false
 	} else if resp.StatusCode == http.StatusForbidden {
+		log.Printf("[-] Manager not found at %s:%d%s\n", tc.host, tc.port, tc.managerPath)
 		tc.managerAvailable = false
 	} else if resp.StatusCode == http.StatusUnauthorized {
 		tc.managerAvailable = true
-		log.Printf("[*] Manager found at %s:%d%s\n", tc.host, tc.port, tc.managerPath)
+		log.Printf("[+] Manager found at %s:%d%s\n", tc.host, tc.port, tc.managerPath)
+	} else if resp.StatusCode == http.StatusBadRequest {
+		tc.check(client, true)
 	} else {
-		fmt.Print(resp.StatusCode)
+		log.Printf("[*] HTTP %d at %s:%d%s\n", resp.StatusCode, tc.host, tc.port, tc.managerPath)
 	}
 	tc.checked = true
 }
 
-func request(tc *tcinstance, username string, password string) (success bool, err error) {
+func request(tc *TcInstance, username string, password string) (success bool, err error) {
 	if !tc.managerAvailable {
 		return false, nil
 	}
 	client := &http.Client{
-		Timeout: time.Second * 15,
+		Timeout: time.Second * 5,
 	}
 	url := buildRequestURL(false, tc.host, tc.port, tc.managerPath)
 	log.Printf("Requesting %s\n", url)
@@ -89,7 +116,7 @@ func request(tc *tcinstance, username string, password string) (success bool, er
 		return false, err
 	}
 	if resp.StatusCode == http.StatusOK {
-		log.Printf("[+] Success! %s:%s on %s\n", username, password, url)
+		log.Printf("[$] Success! %s:%s on %s\n", username, password, url)
 		return true, nil
 	}
 	return false, nil
--- utils.go
+++ utils.go
@@ -1,21 +1,92 @@
 package main
 
 import (
+	"flag"
+	"fmt"
+	"math/rand"
 	"net"
 	"strconv"
+	"strings"
+	"time"
 
 	"github.com/apparentlymart/go-cidr/cidr"
 )
 
-func buildRequestURL(secure bool, host string, port int, managerPath string) string {
-	// TODO: Logic in code to check both regardless of port
-	if secure {
-		return "https://" + host + ":" + strconv.Itoa(port) + managerPath
-	}
-	return "http://" + host + ":" + strconv.Itoa(port) + managerPath
+// TcInstance represents one possible Tomcat Manager instance
+type TcInstance struct {
+	host             string
+	port             uint
+	managerPath      string
+	checked          bool
+	managerAvailable bool
+	finished         bool
 }
 
-func expandNetwork(network *net.IPNet) []net.IP {
+// ScannerConfig holds the global configuration
+type ScannerConfig struct {
+	targetRange []net.IP
+	ports       []uint
+	managerPath string
+	goroutines  uint
+}
+
+func parseCommandLineArgs() ScannerConfig {
+	networkUnparsed := flag.String("target", "", "The target network range in CIDR notation, e.g. 10.10.10.0/24")
+	portsUnparsed := flag.String("ports", "8080,8443,80,443,8000,8888", "Comma separated list of target ports.")
+	managerPathUnparsed := flag.String("managerpath", "/manager/html", "Manager path.")
+	goroutines := flag.Uint("concurrency", 150, "Concurrent Goroutines to use. Due to kernel limitations on linux, it should not be more than 'ulimit -n / 7'.")
+	randomizeHosts := flag.Bool("randomize", true, "Randomize the order that IP:Port is accessed.")
+	flag.Parse()
+	ips := parseStringToNetwork(networkUnparsed, randomizeHosts)
+	ports := parseStringToPorts(portsUnparsed)
+	managerPath := parseStringToManagerPath(managerPathUnparsed)
+	sc := ScannerConfig{ips, ports, managerPath, *goroutines}
+	return sc
+}
+
+func parseStringToManagerPath(rawString *string) string {
+	// Only basic checks to make sure building the request later creates a valid request
+	if strings.HasPrefix(*rawString, "/") {
+		return *rawString
+	}
+	return "/" + *rawString
+}
+
+func parseStringToPorts(rawString *string) []uint {
+	portStringSplitted := strings.Split(*rawString, ",")
+	portSlice := make([]uint, 0)
+	for i := 0; i < len(portStringSplitted); i++ {
+		val, _ := strconv.ParseUint(portStringSplitted[i], 10, 16)
+		portSlice = append(portSlice, uint(val))
+	}
+	return portSlice
+}
+
+func parseStringToNetwork(rawString *string, randomizeHosts *bool) []net.IP {
+	var stringSplitted []string
+	stringSplitted = strings.Split(*rawString, "/")
+	ipString := stringSplitted[0]
+	ipStringSplitted := strings.Split(ipString, ".")
+	if len(ipStringSplitted) != 4 {
+		panic("Error parsing IP address")
+	}
+	netRange, _ := strconv.ParseUint(stringSplitted[1], 10, 8)
+	var ipArr [4]byte
+	for i := uint8(0); i < 4; i++ {
+		val, _ := strconv.ParseUint(ipStringSplitted[i], 10, 8)
+		ipArr[i] = uint8(val)
+	}
+	return expandNetwork(&net.IPNet{IP: net.IPv4(ipArr[0], ipArr[1], ipArr[2], ipArr[3]), Mask: net.CIDRMask(int(netRange), 32)}, *randomizeHosts)
+}
+
+func buildRequestURL(secure bool, host string, port uint, managerPath string) string {
+	if secure {
+		return "https://" + host + ":" + strconv.FormatUint(uint64(port), 10) + managerPath
+	}
+	return "http://" + host + ":" + strconv.FormatUint(uint64(port), 10) + managerPath
+}
+
+func expandNetwork(network *net.IPNet, randomize bool) []net.IP {
 	addrcount := cidr.AddressCount(network)
 	ips := make([]net.IP, 0)
 	nextHost, _ := cidr.Host(network, 0)
@@ -23,5 +94,17 @@ func expandNetwork(network *net.IPNet) []net.IP {
 		ips = append(ips, nextHost)
 		nextHost = cidr.Inc(nextHost)
 	}
+	if randomize {
+		rand.Seed(time.Now().UTC().UnixNano())
+		rand.Shuffle(len(ips), func(i, j int) {
+			ips[i], ips[j] = ips[j], ips[i]
+		})
+	}
 	return ips
+}
+
+// Used to track how long the execution takes
+func timeTrack(start time.Time) {
+	elapsed := time.Since(start)
+	fmt.Printf("Completed in %s", elapsed)
 }
